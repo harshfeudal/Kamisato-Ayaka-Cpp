5421d660093f5d3abdf71fde57c4d2fdc256a74a
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2022-08-19 23:18:01 +0700

    Fixing + updated
    - Edit `.gitignore`
    - Making `premake5.lua`

--- premake5.lua
+++ premake5.lua
@@ -1,1 +1,25 @@
+-- Workspace setup --
+workspace "Kamisato Ayaka"
+    architecture "x86_64"
+    configurations { "Release" }
+
 -- Project setup --
+project "Kamisato Ayaka"
+    kind "ConsoleApp"
+    language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    files
+    {
+        "**.cpp",
+        "commands/**.h",
+        "commands/**.cpp"
+    }
+
+    includedirs
+	{
+		"../include/include-dpp-10.0"
+	}
